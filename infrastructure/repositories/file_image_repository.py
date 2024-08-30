61708cb0d1c019cc14acce2000931e0b4b5da60c
4ytk3 <nuts13718@gmail.com>
2024-08-31 02:30:57 +0900

    remove: Remove image_reader and image_writer and unify them in the repository

--- infrastructure/repositories/file_image_repository.py
+++ infrastructure/repositories/file_image_repository.py
@@ -1,11 +1,17 @@
 from core.entities.image_data import ImageData
 from core.repositories.image_repository import ImageRepository
-from infrastructure.image_reader import ImageReader
-from infrastructure.image_writer import ImageWriter
+import numpy as np
+from PIL import Image
 
 class FileImageRepository(ImageRepository):
     def load_from_file(self, file_path: str) -> ImageData:
-        return ImageReader.read_image(file_path)
+        img = Image.open(file_path)
+        if img.mode == 'RGB':
+            # RGB画像をグレースケールに変換
+            img = img.convert('L')
+        data = np.array(img)
+        return ImageData(data=data, format='grayscale' if img.mode == 'L' else 'RGB')
 
     def save_to_file(self, image_data: ImageData, file_path: str) -> None:
-        ImageWriter.write_image(image_data, file_path)
+        img = Image.fromarray(image_data.get_data())
+        img.save(file_path, format=image_data.get_format())
