105add8bc99387566a1351afb722189e410e6108
4ytk3 <nuts13718@gmail.com>
2024-09-08 03:53:31 +0900

    add: __pycache__

--- domain/entities/image_data.py
+++ domain/entities/image_data.py
@@ -2,31 +2,19 @@ from typing import Tuple
 import numpy as np
 
 class ImageData:
-    def __init__(self, filename: str, data: np.ndarray, format: str = "JPEG", color_space: str = "RGB", ):
-        if not isinstance(data, np.ndarray):
-            raise ValueError("data must be a numpy ndarray")
-        self.filename = filename
-        self.data = data
+    def __init__(self, image: np.ndarray, format: str = "jpg", space: str = "RGB"):
+        self.image = image
         self.format = format
-        self.color_space = color_space
+        self.space = space
 
-    def get_filename(self) -> str:
-        return self.filename
-
-    def get_data(self) -> np.ndarray:
-        return self.data
+    def get_image(self) -> np.ndarray:
+        return self.image
 
     def get_format(self) -> str:
         return self.format
 
-    def get_color_space(self) -> str:
-        return self.color_space
-
-    def get_size(self) -> Tuple[int, int]:
-        if len(self.data.shape) >= 2:
-            return self.data.shape[:2]
-        else:
-            raise ValueError("Invalid image data shape")
+    def get_space(self) -> str:
+        return self.space
 
     def __repr__(self) -> str:
-        return f"ImageData(size={self.get_size()}, format={self.format}, colorspace={self.color_space})"
+        return f"ImageData(name={self.name}, format={self.format}, space={self.space})"
